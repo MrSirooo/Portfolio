8abd6e17a31de2fbab96ced1e419c1eabcb50f68
Iago Otero <iagooteromarcos@gmail.com>
2026-08-04 10:28:31 +0200

    Revert "feat(technical-arsenal): add skill cards with progress indicators"
    This reverts commit 29a82a06e1844a9becb45c187dc9534c566b1865.

---
+++ app/components/technicalArsenal/SkillCard.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+  import type { Skill } from '~/types/Skill'
+  import SkillLevel from './SkillLevel.vue'
+
+  defineProps<{
+    skill: Skill
+  }>()
+</script>
+
+<template>
+  <article class="flex flex-col gap-6 bg-secundary p-6 border-primary hover:border">
+    <header class="flex items-center justify-between">
+      <h3 class="text-2xl font-bold text-primary">
+        {{ skill.name }}
+      </h3>
+
+      <Icon :name="skill.icon" size="28" />
+    </header>
+
+    <SkillLevel :level="skill.level" />
+
+    <p>{{ skill.description }}</p>
+  </article>
+</template>
